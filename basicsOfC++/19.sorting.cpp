736e44d93f63277fb29d6624c6c6a051ae036750
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-01 17:00:18 +0530

    Refactor sorting algorithms for improved readability and consistency

--- basicsOfC++/19.sorting.cpp
+++ basicsOfC++/19.sorting.cpp
@@ -3,48 +3,63 @@
 using namespace std;
 
 // Bubble Sort - O(n^2)
-int bubbleSort (int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+int bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
 }
 
 // Optimized Bubble Sort
-int optimizedBubbleSort (int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
+int optimizedBubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         bool swapped = false;
 
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
 
         // If no two elements were swapped by inner loop, then break the loop
-        if (swapped == false) {
+        if (swapped == false)
+        {
             break;
         }
     }
 }
 
-void printArray (const int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void printArray(const int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
 }
 
 // Selection Sort - O(n^2)
-int selectionSort (int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
+int selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int minIndex = i;
 
-        for (int j = i+1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
                 minIndex = j;
             }
         }
@@ -54,27 +69,31 @@ int selectionSort (int arr[], int n) {
 }
 
 // Insertion Sort - O(n^2)
-int insertionSort (int arr[], int n) {
-    for (int i = 1; i < n; i++) {
+int insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int key = arr[i];
-        int j = i-1;
+        int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j+1] = arr[j];
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
             j--;
         }
 
-        arr[j+1] = key;
+        arr[j + 1] = key;
     }
 }
 
-int main() {
+int main()
+{
     int arr[] = {5, 4, 3, 2, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     bubbleSort(arr, n);
     printArray(arr, n);
     cout << endl;
-    
+
     return 0;
 }
--- problemQuestions/p9.sortColours.cpp
+++ problemQuestions/p9.sortColours.cpp
@@ -7,31 +7,41 @@
 
 using namespace std;
 
-vector<int> sortColorsCount(vector<int>& nums) {
+vector<int> sortColorsCount(vector<int> &nums)
+{
     int n = nums.size();
     int count0 = 0;
     int count1 = 0;
     int count2 = 0;
-    
+
     // Count the number of elements of each colour
-    for (int i = 0; i < n; i++) {
-        if (nums[i] == 0) {
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == 0)
+        {
             count0++;
-        } else if (nums[i] == 1) {
+        }
+        else if (nums[i] == 1)
+        {
             count1++;
-        } else {
+        }
+        else
+        {
             count2++;
         }
     }
 
     // Overwrite the array with the colours
-    for (int i = 0; i < count0; i++) {
+    for (int i = 0; i < count0; i++)
+    {
         nums[i] = 0;
     }
-    for (int i = count0; i < count0 + count1; i++) {
+    for (int i = count0; i < count0 + count1; i++)
+    {
         nums[i] = 1;
     }
-    for (int i = count0 + count1; i < n; i++) {
+    for (int i = count0 + count1; i < n; i++)
+    {
         nums[i] = 2;
     }
 
@@ -39,30 +49,35 @@ vector<int> sortColorsCount(vector<int>& nums) {
 }
 
 // Dutch National Flag Algorithm
-void sortColors(vector<int>& nums) {
+void sortColors(vector<int> &nums)
+{
     int low = 0, mid = 0, high = nums.size() - 1;
-    
-    while (mid <= high) {
-        switch (nums[mid]) {
-            case 0:
-                swap(nums[low++], nums[mid++]);
-                break;
-            case 1:
-                mid++;
-                break;
-            case 2:
-                swap(nums[mid], nums[high--]);
-                break;
+
+    while (mid <= high)
+    {
+        switch (nums[mid])
+        {
+        case 0:
+            swap(nums[low++], nums[mid++]);
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(nums[mid], nums[high--]);
+            break;
         }
     }
 }
 
-int main() {
+int main()
+{
     vector<int> nums = {2, 0, 2, 1, 1, 0};
     vector<int> result = sortColorsCount(nums);
     // Time Complexity - O(n)
 
-    for (int i = 0; i < result.size(); i++) {
+    for (int i = 0; i < result.size(); i++)
+    {
         cout << result[i] << " ";
     }
     cout << endl;
