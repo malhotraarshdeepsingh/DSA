ddc712d5df8d2e2b193e887db4ea1ddeba5aa43a
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-02 16:23:22 +0530

    Refactor formatting in priority queue implementation

--- basicsOfC++/25.priorityQueue.cpp
+++ basicsOfC++/25.priorityQueue.cpp
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     // create a max heap priority queue
     priority_queue<int> pq;
-    
+
     // syntax for min heap priority
     // priority_queue<int, vector<int>, greater<int>> pq;
 
@@ -17,20 +18,20 @@ int main() {
     pq.push(20);
     pq.push(5);
     pq.push(15);
-    
+
     cout << "Size of the priority queue: " << pq.size() << endl;
-    
+
     cout << "Top element of the priority queue: " << pq.top() << endl;
-    
+
     pq.pop();
 
     cout << "Size of the priority queue after popping: " << pq.size() << endl;
     cout << "Top element of the priority queue after popping: " << pq.top() << endl;
 
-    // inbuilt functions are 
+    // inbuilt functions are
     // push, pop, top, size, empty, swap, emplace
     // push and pop has O(log n) time complexity because it sorts before inserting
     // top has O(1) time complexity
-    
+
     return 0;
 }
