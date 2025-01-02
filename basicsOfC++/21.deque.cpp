2a424b1b7d83f7eae0993d6fb98e42c960b41ca3
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-02 16:14:43 +0530

    Refactor formatting and improve readability in deque and stack implementations

--- basicsOfC++/21.deque.cpp
+++ basicsOfC++/21.deque.cpp
@@ -4,36 +4,37 @@
 // Deque is similar to vector but more efficient in case of insertion and deletion at both ends.
 // Deque is a combination of stack and queue.
 
-#include<iostream>
-#include<deque>
+#include <iostream>
+#include <deque>
 
 using namespace std;
 
-int main() {
+int main()
+{
     deque<int> dq = {0};
-    
-    // Insertion at both ends 
+
+    // Insertion at both ends
     dq.push_front(1);
     dq.push_back(2);
     dq.push_front(3);
     dq.push_back(4);
-    
+
     cout << "Deque elements: ";
-    for(int i = 0; i < dq.size(); i++)
+    for (int i = 0; i < dq.size(); i++)
         cout << dq[i] << " ";
     cout << endl;
-    
+
     // Deletion at both ends
     dq.pop_front();
     dq.pop_back();
-    
+
     cout << "After deletion: ";
-    for(int i = 0; i < dq.size(); i++)
+    for (int i = 0; i < dq.size(); i++)
         cout << dq[i] << " ";
     cout << endl;
 
     // all inbuilt functions work with deque
     // size, erase, clear, begin, end, rbegin, rend, insert, front, back, empty, remove, sort, reverse, unique, merge, splice, swap, assign, resize, push_front, push_back, pop_front, pop_back, max_size, remove_if
-    
+
     return 0;
 }
--- basicsOfC++/23.stacks.cpp
+++ basicsOfC++/23.stacks.cpp
@@ -2,7 +2,8 @@
 #include <stack>
 using namespace std;
 
-int main() {
+int main()
+{
     stack<int> s;
 
     // Stacks follow the principle of LIFO (Last In First Out) and are only accessible from one side of the container.
@@ -15,29 +16,32 @@ int main() {
     // inbuilt functions are
     // push, emplace, top, pop, size, empty, swap
 
-    while (!s.empty()) {
+    while (!s.empty())
+    {
         cout << s.top() << " ";
         s.pop(); // removes the top element
     }
     cout << endl;
 
-    stack <int> s1, s2;
+    stack<int> s1, s2;
 
     s1.push(10);
     s1.push(20);
     s1.push(30);
-    
+
     s2.swap(s1);
 
     cout << "Stack 1: ";
-    while (!s1.empty()) {
+    while (!s1.empty())
+    {
         cout << s1.top() << " ";
         s1.pop();
     }
     cout << endl;
 
     cout << "Stack 2: ";
-    while (!s2.empty()) {
+    while (!s2.empty())
+    {
         cout << s2.top() << " ";
         s2.pop();
     }
