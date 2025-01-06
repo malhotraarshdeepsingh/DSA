4a03d57086090147aee51d31b96b6259b49347dd
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-06 17:55:03 +0530

    Add implementation of searchMatrix function for searching in a 2D matrix

--- problemQuestions/p17.searchIn2DMatrix.cpp
+++ problemQuestions/p17.searchIn2DMatrix.cpp
@@ -35,6 +35,36 @@ bool searchInMatrix(vector<vector<int>> &matrix, int k)
         else
             startRow = midRow + 1;
     }
-    
+
     return false;
 }
+
+// Time Complexity :- O(log(n) + log(m))
+// Space Complexity :- O(1)
+
+// Given a matrix of type 
+// 1   4   7   11  15
+// 2   5   8   12  19
+// 3   6   9   16  22
+// 10  13  14  17  24
+// 18  21  23  26  30
+// and k = 5
+
+bool searchMatrix(vector<vector<int>> &matrix, int k)
+{
+    int m = matrix.size(), n = matrix[0].size();
+    int r = 0, c = n - 1;
+
+    while(r < m && c >= 0){
+        if (matrix[r][c] == k)
+            return true;
+        else if (matrix[r][c] < k)
+            r++;
+        else
+            c--;
+    }
+
+    return false;
+}
+
+// Time Complexity :- O(n + m)
