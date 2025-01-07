e91d39416b4f3e84d15bd032c9108c2f712d31d1
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-07 17:13:16 +0530

    Add function to find all unique combinations that sum to a target

---
+++ problemQuestions/p30.combinationSum.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+set<vector<int>> s;
+
+void getAllCombinations(vector<int> &arr, int target, int index, vector<int> &currentCombination, vector<vector<int>> &result)
+{
+    if (target < 0 || index == arr.size())
+        return;
+
+    if (target == 0)
+    {
+        if (s.find(currentCombination) == s.end())
+        {
+            result.push_back(currentCombination);
+            s.insert(currentCombination);
+            return;
+        }
+    }
+
+    currentCombination.push_back(arr[index]);
+    // single
+    getAllCombinations(arr, target - arr[index], index + 1, currentCombination, result);
+    // multiple
+    getAllCombinations(arr, target - arr[index], index, currentCombination, result);
+    currentCombination.pop_back();
+
+    // exclusion
+    getAllCombinations(arr, target, index + 1, currentCombination, result);
+}
+vector<vector<int>> CombinationsSum(vector<int> &arr, int target)
+{
+    vector<vector<int>> result;
+    vector<int> currentCombination;
+
+    getAllCombinations(arr, target, 0, currentCombination, result);
+
+    return result;
+}
