2f15256250466821bbf22b38a0332abd5ca96dae
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-09 15:25:03 +0530

    Add implementation of function to detect cycle in a linked list

---
+++ problemQuestions/p36.cycleLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head)
+{
+    if (head == NULL)
+        return false;
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
