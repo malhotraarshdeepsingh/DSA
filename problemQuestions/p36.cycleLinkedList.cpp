c776177870b27316e8471d96f1134fe7ab513a96
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-01-09 15:26:02 +0530

    Add implementation of function to detect the starting node of a cycle in a linked list

--- problemQuestions/p36.cycleLinkedList.cpp
+++ problemQuestions/p36.cycleLinkedList.cpp
@@ -28,3 +28,32 @@ bool hasCycle(ListNode *head)
 
     return false;
 }
+
+ListNode *detectCycle(ListNode *head)
+{
+    if (head == NULL)
+        return NULL;
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+
+            return slow;
+        }
+    }
+
+    return NULL;
+}
