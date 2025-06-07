cce743cc8554d3d978acbb7671f5cc4c08bf7073
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-06-07 11:09:42 +0530

    Add top view traversal function for binary tree implementation

--- problemQuestions/p66.topViewofBinaryTree.cpp
+++ problemQuestions/p66.topViewofBinaryTree.cpp
@@ -16,19 +16,19 @@ public:
         if (!root) return;
 
         if (m.find(level) == m.end()) {
-            m[level] = {root->val, level}; // Store value and level
+            m[level] = {root->val, level};
         }
 
         TopView(root->left, level + 1, m);
         TopView(root->right, level + 1, m);
     }
     vector<int> topView(TreeNode *root) {
-        map<int, pair<int, int>> m; // Map to store value and level
+        map<int, pair<int, int>> m; 
         TopView(root, 0, m);
 
         vector<int> result;
         for (auto &it : m) {
-            result.push_back(it.second.first); // Push only the value
+            result.push_back(it.second.first);
         }
         return result;
     }
