f66f8bdbbbc375db1ce38602f0fe116d1f62ff19
Arshdeep Singh Malhotra <malhotraarshdeepsingh@gmail.com>
2025-06-08 11:53:25 +0530

    Refactor Node and Solution class definitions for consistency in formatting

--- problemQuestions/p67.kthLevel.cpp
+++ problemQuestions/p67.kthLevel.cpp
@@ -2,24 +2,29 @@
 
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
     Node *left, *right;
-    
+
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
 void kthLevel(Node *root, int k)
 {
-    if (root == nullptr) {
+    if (root == nullptr)
+    {
         return;
     }
-    
-    if (k == 0) {
+
+    if (k == 0)
+    {
         cout << root->data << " ";
         return;
     }
-    
+
     kthLevel(root->left, k - 1);
     kthLevel(root->right, k - 1);
 }
+
+// Time Complexity: O(N), Space Complexity: O(N) 
--- problemQuestions/p68.LCA.cpp
+++ problemQuestions/p68.LCA.cpp
@@ -2,31 +2,39 @@
 
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
     Node *left, *right;
-    
+
     Node(int val) : data(val), left(nullptr), right(nullptr) {}
 };
 
-class Solution {
+class Solution
+{
 public:
-    Node* LCA(Node* root, Node* n1, Node* n2) {
-        if (!root) {
+    Node *LCA(Node *root, Node *n1, Node *n2)
+    {
+        if (!root)
+        {
             return nullptr;
         }
 
-        if (root->data == n1->data || root->data == n2->data) {
+        if (root->data == n1->data || root->data == n2->data)
+        {
             return root;
         }
 
-        Node* left = LCA(root->left, n1, n2);
-        Node* right = LCA(root->right, n1, n2);
+        Node *left = LCA(root->left, n1, n2);
+        Node *right = LCA(root->right, n1, n2);
 
-        if (left && right) {
+        if (left && right)
+        {
             return root;
         }
-        
+
         return left ? left : right;
     }
 };
+
+// Time Complexity: O(N), Space Complexity: O(N)
